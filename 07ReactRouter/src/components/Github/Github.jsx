cd062deb332dedd57ed4bc6ca1dd16afd7f68def
adi32p <aditya.22210268@viit.ac.in>
2025-02-16 15:51:07 +0530

    reactRouter

---
+++ 07ReactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,21 @@
+// import { response } from 'express'
+// import React, { useEffect, useState } from 'react'
+
+// function Github() {
+//     const[data,setData] = useState([])
+//     useEffect(()=>{
+//         fetch('https://api.github.com/users/adip32')
+//         .then(response=>response.json)
+//         .then(data=>{
+//             console.log(data);
+//             setData(data)
+//         })
+//     },[])
+//   return (
+//     <div className='bg-gray-600 text-white text-center m-4 p-3 text-3xl'>
+//       Github Followers:{data.followers}
+//     </div>
+//   )
+// }
+
+// export default Github
